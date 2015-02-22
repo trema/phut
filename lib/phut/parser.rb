8efee42b22cecf5548067045cc56ba04da5645c4
Yasuhito Takamiya <yasuhito@gmail.com>
2015-02-22 18:40:51 +0900

    Refactoring.

--- lib/phut/parser.rb
+++ lib/phut/parser.rb
@@ -11,33 +11,41 @@ module Phut
 
     def parse(file)
       Syntax.new(@config).instance_eval IO.read(file), file
-      set_vswitch_interfaces
-      set_vhost_interface
+      assign_vswitch_interfaces
+      assign_vhost_interface
       @config
     end
 
     private
 
-    def set_vswitch_interfaces
+    def assign_vswitch_interfaces
       @config.vswitches.each do |each|
         each.interfaces = find_interfaces_by_name(each.name)
       end
     end
 
-    def set_vhost_interface
+    def assign_vhost_interface
       @config.vhosts.each do |each|
-        interface = find_interfaces_by_name(each.name)
-        fail "No link found for host #{each.name}" if interface.empty?
-        if interface.size > 1
-          fail "Multiple links are connected to host #{each.name}"
-        end
-        each.interface = interface.first
+        each.interface = find_host_interface_by_name(each.name)
       end
     end
 
     def find_interfaces_by_name(name)
-      @config.links.select { |each| each.name_a == name }.map(&:device_a) +
-        @config.links.select { |each| each.name_b == name }.map(&:device_b)
+      find_device_by_name(name, :name_a, :device_a) +
+        find_device_by_name(name, :name_b, :device_b)
+    end
+
+    def find_host_interface_by_name(name)
+      find_interfaces_by_name(name).tap do |interface|
+        fail "No link found for host #{name}" if interface.empty?
+        fail "Multiple links connect to host #{name}" if interface.size > 1
+      end.first
+    end
+
+    def find_device_by_name(name, name_type, device_type)
+      @config.links.select do |each|
+        each.__send__(name_type) == name
+      end.map(&device_type)
     end
   end
 end
