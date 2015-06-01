e7594acfac1b00602005988ee4ae71820c4c2844
Yasuhito Takamiya <yasuhito@gmail.com>
2015-06-01 10:04:21 +0900

    Add port attribute to vswitch directive.

--- features/step_definitions/phut_steps.rb
+++ features/step_definitions/phut_steps.rb
@@ -10,6 +10,13 @@ Then(/^a vswitch named "(.*?)" should be running$/) do |name|
   expect(system("sudo ovs-vsctl br-exists br#{name}")).to be_truthy
 end
 
+# rubocop:disable LineLength
+Then(/^a vswitch named "([^"]*)" \(controller port = (\d+)\) should be running$/) do |name, port_number|
+  step %(a vswitch named "#{name}" should be running)
+  step %(the output should contain "ovs-vsctl set-controller br#{name} tcp:127.0.0.1:#{port_number}")
+end
+# rubocop:enable LineLength
+
 Then(/^a vswitch named "(.*?)" should not be running$/) do |name|
   expect(system("sudo ovs-vsctl br-exists br#{name}")).to be_falsey
 end
