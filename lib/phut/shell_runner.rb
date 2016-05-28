9c62c74cc925db858f6c131cfe3568fa88de284c
Yasuhito Takamiya <yasuhito@gmail.com>
2016-05-28 12:54:54 +0900

    Refactor Phut::ShellRunner

--- lib/phut/shell_runner.rb
+++ lib/phut/shell_runner.rb
@@ -1,9 +1,18 @@
+# frozen_string_literal: true
+require 'open3'
+
 module Phut
   # Provides sh method.
   module ShellRunner
+    def sudo(command)
+      sh "sudo #{command}"
+    end
+
     def sh(command)
-      system(command) || fail("#{command} failed.")
       @logger.debug(command) if @logger
+      stdout, stderr, status = Open3.capture3(command)
+      fail %(Command '#{command}' failed: #{stderr}) unless status.success?
+      stdout
     end
   end
 end
